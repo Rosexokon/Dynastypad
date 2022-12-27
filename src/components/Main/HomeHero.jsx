2cafac71b17d256412ee3221f97c1e05280d4687
BrightTechnologies1 <silascyrax@gmail.com>
2022-12-27 13:59:55 +0100

    updated hero

--- src/components/Main/HomeHero.jsx
+++ src/components/Main/HomeHero.jsx
@@ -12,17 +12,15 @@ function HomeHero() {
         <div className="col-lg-6 col-md-6 col-sm-12 p-8">
           <div className="font-[syne] header-text">
             <h1>Discover, Buy, Add and Curate <span className='text-[#4176FF]'>NFTs</span> Easily</h1>
-            <p>Join users who use DynastyPad, the #1 Curate-and-Earn Platform to buy NEAR nfts from one place, and earn rewards for curating.</p>
+            {/* <p>Join users who use DynastyPad, the #1 Curate-and-Earn Platform to buy NEAR nfts from one place, and earn rewards for curating.</p> */}
           </div>          
           <div className="header-btn mt-4">
-            <button className='btn'>Browse Collection</button>
-            <button className='btn btn2'>Apply for Listing</button>
+            <button className='btn px-3 py-2 rounded-md '>Browse Collection</button>
+            <button className='btn btn2 px-3 py-2 rounded-md '>Apply for Listing</button>
           </div>
         </div>
-        <div className="col-lg-6 col-md-6 col-sm-12 p-8">
-          <div className='header-img'>
-            <img src={bgcard} alt="" srcset="" width={400} />
-          </div>
+        <div className='header-img col-lg-6 col-md-6 col-sm-12 p-8'>
+          <img src={bgcard} alt="" srcset="" width={500} />
         </div>
       </div>
       
